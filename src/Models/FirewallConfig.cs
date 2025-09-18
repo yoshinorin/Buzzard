4d812aab98daef7033b5d1e280a431bc94908600
yoshinorin <yoshinorin.net@outlook.com>
2025-09-19 00:25:22 +0900

    refactor(waf/rules): extract `PathConfig` classes to separate file

--- src/Models/FirewallConfig.cs
+++ src/Models/FirewallConfig.cs
@@ -4,16 +4,3 @@ public class FirewallConfig
 {
     public PathConfig Path { get; set; } = new();
 }
-
-public class PathConfig
-{
-    public PathRules Allow { get; set; } = new();
-    public PathRules Deny { get; set; } = new();
-}
-
-public class PathRules
-{
-    public List<string> Contains { get; set; } = new();
-    public List<string> StartsWith { get; set; } = new();
-    public List<string> EndsWith { get; set; } = new();
-}
---
+++ src/Models/PathConfig.cs
@@ -0,0 +1,14 @@
+namespace Buzzard.Models;
+
+public class PathConfig
+{
+    public PathRules Allow { get; set; } = new();
+    public PathRules Deny { get; set; } = new();
+}
+
+public class PathRules
+{
+    public List<string> Contains { get; set; } = new();
+    public List<string> StartsWith { get; set; } = new();
+    public List<string> EndsWith { get; set; } = new();
+}
